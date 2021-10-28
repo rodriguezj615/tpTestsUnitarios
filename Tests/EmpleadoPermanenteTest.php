596147184ef346c211b14c66c827aabd25e5a4dd
Juanchi <rodriguezj615@hotmail.com>
2021-10-28 16:24:31 -0300

    Update EmpleadoPermanenteTest.php

--- Tests/EmpleadoPermanenteTest.php
+++ Tests/EmpleadoPermanenteTest.php
@@ -20,6 +20,37 @@
 			$empperm= $this->crear();
 			$this->assertEquals($hoy->format('Y-m-d'), $empperm->getFechaIngreso()->format('Y-m-d'));
 		}
-    
+        
+    		//2 - crear y obtener calculo de Comision
+		public function testCalculoDeComisionEnBaseALaAntiguedad(){
+			$ingreso = new DateTime();
+			$ingreso->modify('-10 years');
+			$empperm= $this->crear('Juan','Rodriguez','37701467','5000', $ingreso); 
+			$this->assertEquals("10%",$empperm->calcularComision());
+		}
+
+        // 3 - crear y obtener calculo de Ingreso Total
+		public function testCalculoDeIngresoTotal(){
+			$ingreso = new DateTime();
+			$ingreso->modify('-10 years');
+			$empperm= $this->crear('Juan','Rodriguez','37701467','5000', $ingreso); 
+			$this->assertEquals(5500,$empperm->calcularIngresoTotal());
+		}
+
+		// 4 - crear y obtener calculo de Antiguedad 
+		public function testSePuedeCalcularAntiguedad(){
+			$ingreso = new DateTime();
+			$ingreso->modify('-10 years');
+			$empperm= $this->crear('Juan','Rodriguez','37701467','5000', $ingreso);
+			$this->assertEquals(10,$empperm->calcularAntiguedad());
+		}
+
+        // 5 - Fecha de ingreso posterior a la de hoy, excepción
+		public function testNoSeCreaConFechaPosteriorAHoy(){
+			$ingreso = new DateTime();
+			$ingreso->modify('+10 years');
+			$this->expectException(\Exception::class);
+			$empperm= $this->crear('Juan','Rodriguez','37701467','5000', $ingreso);
+		}
     }
 ?>
